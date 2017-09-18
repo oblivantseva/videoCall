15371af6bb08e117b358bb06bdaa6fcf98f8cc76
kom <genyweb@mail.ru>
2017-09-18 18:29:20 +0500

    interface

--- App/App/Form1.Designer.cs
+++ App/App/Form1.Designer.cs
@@ -53,10 +53,11 @@
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.Color.AliceBlue;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox1.ForeColor = System.Drawing.Color.Red;
             this.textBox1.Location = new System.Drawing.Point(198, 45);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(393, 20);
+            this.textBox1.Size = new System.Drawing.Size(393, 21);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "Название мероприятия";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -64,10 +65,11 @@
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.Color.AliceBlue;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox2.ForeColor = System.Drawing.Color.Red;
             this.textBox2.Location = new System.Drawing.Point(296, 81);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(179, 20);
+            this.textBox2.Size = new System.Drawing.Size(179, 21);
             this.textBox2.TabIndex = 2;
             this.textBox2.Text = "Статус мероприятия";
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -77,14 +79,16 @@
             this.listBox1.BackColor = System.Drawing.Color.AliceBlue;
             this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(25, 114);
+            this.listBox1.Location = new System.Drawing.Point(25, 107);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(265, 145);
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(243, 145);
             this.listBox1.TabIndex = 3;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.LightGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button1.ForeColor = System.Drawing.Color.Red;
             this.button1.Location = new System.Drawing.Point(324, 124);
             this.button1.Name = "button1";
@@ -96,6 +100,7 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.LightGreen;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button2.ForeColor = System.Drawing.Color.Red;
             this.button2.Location = new System.Drawing.Point(324, 177);
             this.button2.Name = "button2";
@@ -150,7 +155,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(746, 320);
+            this.ClientSize = new System.Drawing.Size(710, 319);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
