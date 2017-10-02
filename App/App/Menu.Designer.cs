b4e9d5af1d14762eaa96d4407a4e8590d0cfcbf2
kate <ket54479120@yandex.ru>
2017-10-03 00:22:21 +0600

    опять формы

--- App/App/Menu.Designer.cs
+++ App/App/Menu.Designer.cs
@@ -41,34 +41,34 @@
             // pass
             // 
             this.pass.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.pass.ForeColor = System.Drawing.Color.Black;
-            this.pass.Location = new System.Drawing.Point(178, 38);
+            this.pass.Location = new System.Drawing.Point(34, 44);
             this.pass.Name = "pass";
             this.pass.PasswordChar = '*';
-            this.pass.Size = new System.Drawing.Size(136, 20);
+            this.pass.Size = new System.Drawing.Size(136, 26);
             this.pass.TabIndex = 11;
             this.pass.Text = "******";
             // 
             // login
             // 
             this.login.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.login.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.login.ForeColor = System.Drawing.Color.Black;
-            this.login.Location = new System.Drawing.Point(177, 12);
+            this.login.Location = new System.Drawing.Point(34, 12);
             this.login.Name = "login";
-            this.login.Size = new System.Drawing.Size(136, 20);
+            this.login.Size = new System.Drawing.Size(136, 26);
             this.login.TabIndex = 10;
             this.login.Text = "Логин";
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.Blue;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button3.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.button3.Location = new System.Drawing.Point(209, 68);
+            this.button3.Location = new System.Drawing.Point(188, 10);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(106, 25);
+            this.button3.Size = new System.Drawing.Size(106, 30);
             this.button3.TabIndex = 9;
             this.button3.Text = "Войти";
             this.button3.UseVisualStyleBackColor = false;
@@ -76,59 +76,76 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 68);
+            this.button1.BackColor = System.Drawing.Color.Blue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(299, 76);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(176, 23);
+            this.button1.Size = new System.Drawing.Size(238, 31);
             this.button1.TabIndex = 12;
             this.button1.Text = "Модерация обращений";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 97);
+            this.button2.BackColor = System.Drawing.Color.Blue;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(299, 121);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(176, 23);
+            this.button2.Size = new System.Drawing.Size(238, 31);
             this.button2.TabIndex = 13;
             this.button2.Text = "Эфирные обращения";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(12, 126);
+            this.button4.BackColor = System.Drawing.Color.Blue;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(299, 166);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(176, 23);
+            this.button4.Size = new System.Drawing.Size(238, 31);
             this.button4.TabIndex = 14;
             this.button4.Text = "Обращения-звонок";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(12, 155);
+            this.button5.BackColor = System.Drawing.Color.Blue;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.Location = new System.Drawing.Point(299, 211);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(176, 23);
+            this.button5.Size = new System.Drawing.Size(238, 31);
             this.button5.TabIndex = 15;
             this.button5.Text = "Управление мероприятиями";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(12, 184);
+            this.button6.BackColor = System.Drawing.Color.Blue;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button6.Location = new System.Drawing.Point(299, 256);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(176, 23);
+            this.button6.Size = new System.Drawing.Size(238, 31);
             this.button6.TabIndex = 16;
             this.button6.Text = "Стартовая";
-            this.button6.UseVisualStyleBackColor = true;
+            this.button6.UseVisualStyleBackColor = false;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(325, 328);
+            this.BackgroundImage = global::App.Properties.Resources.фон;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(560, 325);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
