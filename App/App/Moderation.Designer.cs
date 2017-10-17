9e6aa9a1f0ffb129ee7601efb92cf53bf53ba0ad
ASOblivantseva <o9a7@mail.ru>
2017-10-17 18:36:28 +0500

    old

--- App/App/Moderation.Designer.cs
+++ App/App/Moderation.Designer.cs
@@ -241,6 +241,7 @@
             this.button2.TabIndex = 17;
             this.button2.Text = "В обработку";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
